0ca8aeb07c802e9d066c0ee9b2e5b0afaf829056
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-12 21:55:22 -0300

    feat: initial loading

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 import Logo from "../../assets/logo.svg";
 import { Wrapper } from "./styled";
+
 const Welcome = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/home");
+    }, 2000);
+  }, []);
   return (
     <Wrapper>
       <img src={Logo} />
